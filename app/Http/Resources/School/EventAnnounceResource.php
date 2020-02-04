1dc8bf920ae682ebfd997cdb2d3d9c90b579e809
NDS <dddddd1997@gmail.com>
2020-02-04 19:27:43 +0300

    eventAnnounces fix

--- app/Http/Resources/School/EventAnnounceResource.php
+++ app/Http/Resources/School/EventAnnounceResource.php
@@ -14,6 +14,6 @@ class EventAnnounceResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [parent::toArray($request)];
+        return parent::toArray($request);
     }
 }
