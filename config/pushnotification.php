9c45dae2716c92cccf7926b13d5dfbcb6525fa33
Vladislav <vladislavmatkovskiy2015@mail.ru>
2020-09-15 09:21:18 +0300

    added server push key

--- config/pushnotification.php
+++ config/pushnotification.php
@@ -12,7 +12,7 @@ return [
     'fcm' => [
         'priority' => 'normal',
         'dry_run' => false,
-        'apiKey' => 'My_ApiKey',
+        'apiKey' => 'AAAAIrALqH0:APA91bFiotGaPtGC6kNEjBWGRFZq8jJkSDUH_kqOcYAB582EufnrjMJa71IYQRDPPz1GHmt3l4xQHLt-lccva0i-ITQiRrUCDqc-w7oBJZJZZr4Rkt-4TrW5bSXTrD0O_JJgjkpKzfme',
     ],
     'apn' => [
         'certificate' => __DIR__ . '/iosCertificates/apns-dev-cert.pem',
